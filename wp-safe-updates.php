fc9f40f4803dd7b7838068f902a72f2374f796f8
Antti Kuosmanen <antti@seravo.fi>
2016-07-04 17:04:36 +0300

    Make "unit tests" output prettier

--- wp-safe-updates.php
+++ wp-safe-updates.php
@@ -54,10 +54,10 @@ class Safe_Updates {
       print_r( $alt_heap->get_wp_tables() );
 
       echo "Cloning to 'test'... \n";
-      print_r( $alt_heap->clone_wp_tables('test') );
+      $alt_heap->clone_wp_tables('test');
 
       echo "Cloning to 'test2'... \n";
-      print_r( $alt_heap->clone_wp_tables('test2') );
+      $alt_heap->clone_wp_tables('test2');
 
       echo "All tmp tables: \n";
       print_r( $alt_heap->get_tmp_wp_tables() );
@@ -66,25 +66,28 @@ class Safe_Updates {
       print_r( $alt_heap->get_tmp_wp_tables('test') );
 
       echo "Creating plugins directory for 'test'... \n";
-      print_r( $alt_heap->create_alt_plugins_dir('test') );
+      $alt_heap->create_alt_plugins_dir('test');
 
       echo "Creating plugins directory for 'test2'... \n";
-      print_r( $alt_heap->create_alt_plugins_dir('test2') );
+      $alt_heap->create_alt_plugins_dir('test2');
 
       echo "Alt plugin dirs: \n";
       print_r( $alt_heap->get_alt_plugins_dirs() );
 
-      echo "'Test' plugin dirs: \n";
+      echo "'Test' plugin dir: \n";
       print_r( $alt_heap->get_alt_plugins_dirs('test') );
 
       echo "Deleting 'test' tables... \n";
-      print_r( $alt_heap->delete_tmp_wp_tables('test') );
+      $alt_heap->delete_tmp_wp_tables('test');
 
       echo "Deleting 'test' plugins directory... \n";
-      print_r( $alt_heap->delete_alt_plugins_dirs('test') );
+      $alt_heap->delete_alt_plugins_dirs('test');
+
+      echo "Alt plugin dirs: \n";
+      print_r( $alt_heap->get_alt_plugins_dirs() );
 
       echo "Deleting all tmp plugins directories... \n";
-      print_r( $alt_heap->delete_alt_plugins_dirs() );
+      $alt_heap->delete_alt_plugins_dirs();
 
       echo "Alt plugin dirs: \n";
       print_r( $alt_heap->get_alt_plugins_dirs() );
@@ -93,7 +96,7 @@ class Safe_Updates {
       print_r( $alt_heap->get_tmp_wp_tables() );
 
       echo "Deleting all tmp tables... \n";
-      print_r( $alt_heap->delete_tmp_wp_tables() );
+      $alt_heap->delete_tmp_wp_tables();
 
       echo "All tmp tables: \n";
       print_r( $alt_heap->get_tmp_wp_tables() );
