a7641b24fa55d762bdaa321d076493e5818c9c43
Antti Kuosmanen <antti@seravo.fi>
2016-07-08 10:52:21 +0300

    Tagging 1.0.2-alpha

--- wp-safe-updates.php
+++ wp-safe-updates.php
@@ -3,7 +3,7 @@
  * Plugin name: WP Safe Updates
  * Plugin URI: https://github.com/anttiviljami/wp-safe-updates
  * Description: Test WordPress plugin updates safely before applying them on the live site.
- * Version: 1.0.1
+ * Version: 1.0.2-alpha
  * Author: @anttiviljami
  * Author URI: https://github.com/anttiviljami
  * License: GPLv3
